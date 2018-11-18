2954682573fdb9f41ef91715e515c5d4d2c139dd
ChenDomy <17875511974@163.com>
2018-11-18 21:17:31 +0800

    数据库配置文件

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 数据库配置文件
+ */
+return [
+  "host"          => "localhost",
+  "port"          => 3306,
+  "database"      => "download",
+  "username"      => "root",
+  "password"      => "",
+  "charset"       => "utf8",
+  "prefix"        => "dload_"
+];
